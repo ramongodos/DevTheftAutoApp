3cbc525dd48488915ee912c2e804a9f6c22bf56a
Ramon Godos <ramon@ramon.local>
2026-05-21 10:33:31 +0200

    Guardado temporal antes de hacer pull

---
+++ backend/devtheftauto/src/main/java/devtheftauto/devtheftauto/DevtheftautoApplication.java
@@ -0,0 +1,13 @@
+package devtheftauto.devtheftauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtheftautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtheftautoApplication.class, args);
+	}
+
+}
---
+++ backend/devtheftauto/src/test/java/devtheftauto/devtheftauto/DevtheftautoApplicationTests.java
@@ -0,0 +1,13 @@
+package devtheftauto.devtheftauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtheftautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
