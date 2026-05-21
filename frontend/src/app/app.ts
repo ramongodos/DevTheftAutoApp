2ce4d3c32db7c570957857dc8adae595f84cb71b
Betsaida <betsaidasevifer@gmail.com>
2026-05-21 12:42:09 +0200

    Cambios en app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar'; // Ruta directa sin components
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  template: `
+    <nav style="background: #2f3542; padding: 15px; display: flex; gap: 20px; font-family: sans-serif;">
+      <a routerLink="/" routerLinkActive="active" style="color: white; text-decoration: none; font-weight: bold;">🏠 Home</a>
+      <a routerLink="/catalogo" routerLinkActive="active" style="color: white; text-decoration: none; font-weight: bold;">🎮 Catálogo</a>
+      <a routerLink="/agregar" routerLinkActive="active" style="color: white; text-decoration: none; font-weight: bold;">➕ Añadir Juego</a>
+    </nav>
+  `
 })
-export class App { }
+export class NavbarComponent { }
