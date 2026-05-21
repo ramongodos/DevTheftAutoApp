b6fdf3205715acbfe77a76ddf327a6347152fce4
Betsaida <betsaidasevifer@gmail.com>
2026-05-21 12:52:51 +0200

    Cambios enp.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { CatalogoComponent } from './catalogo/catalogo.component';
-import { DetalleComponent } from './detalle/detalle';
-import { AgregarComponent } from './agregar/agregar';
-import { HomeComponent } from './home/home';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { DetalleComponent } from './components/catalogo/detalle.component'; // Apunta a donde está realmente metido en vuestro árbol
+import { AgregarComponent } from './components/agregar/agregar';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: CatalogoComponent },
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'detalle', component: DetalleComponent },
   { path: 'agregar', component: AgregarComponent },
