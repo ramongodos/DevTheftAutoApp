d68b8eb2d4fb3dda29bb8aa9274651e87768ca5e
Betsaida <betsaidasevifer@gmail.com>
2026-05-21 11:08:37 +0200

    Estructura oficial de Angular CLI generada con éxito

---
+++ frontend/src/app/components/catalogo/catalogo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  imports: [],
+  template: ` <p>catalogo works!</p> `,
+  styles: ``,
+})
+export class Catalogo {}
